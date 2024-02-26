221b4257f6c10db235c937c5a4c24040f8e339ea
Hizbucodes <106305260+Hizbucodes@users.noreply.github.com>
2024-02-27 02:49:54 +0530

    added-login-registration using firebase

--- src/components/cart-components/SideBar.jsx
+++ src/components/cart-components/SideBar.jsx
@@ -13,7 +13,7 @@ const SideBar = () => {
   }, [cart]);
 
   return (
-    <div className='pt-28 min-h-screen bg-blue-100 w-[20%] flex flex-col gap-12 p-5'>
+    <div className='pt-28 min-h-screen shadow-2xl w-[20%] flex flex-col gap-12 p-5'>
         <div className='font-bold text-2xl '>
         <h1>Subtotal ({cart.length}) items</h1>
         
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKjM1Hj1PqI-F44FY3N8FQSZsnVUQzxf4",
+  authDomain: "cafe-web-app-6af28.firebaseapp.com",
+  projectId: "cafe-web-app-6af28",
+  storageBucket: "cafe-web-app-6af28.appspot.com",
+  messagingSenderId: "583440162315",
+  appId: "1:583440162315:web:a7405b6ebeae08d684710e"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
